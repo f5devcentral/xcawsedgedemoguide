5b9b7db3f851ed4ff8a9bb68f1021be82879a17c
Alex Shemyakin <a.shemyakin@f5.com>
2025-08-30 00:24:52 +0200

    upgrade tf dependencies

--- terraform/aws-app-stack-branch/providers.tf
+++ terraform/aws-app-stack-branch/providers.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     volterra = {
       source = "volterraedge/volterra"
-      version = "=0.11.22"
+      version = "=0.11.44"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "=4.67.0"
+      version = "=6.11.0"
     }
   }
 }
